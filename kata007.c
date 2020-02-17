c92e8d0f65cfeffbbfd089669fdb660533729c71
1432junaid <1432junaid@gmail.com>
2020-02-17 19:56:48 +0530

    updates

---
+++ kata007.c
@@ -0,0 +1,16 @@
+//find the frequency of the letters in a string
+
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"Enter a string"<<endl;
+	char a[10];
+	int len = sizeof(a)/sizeof(a[0]);
+	for(int i=0;i<len<i++){
+		cin>>&a[i];
+	}
+	for(int j=0;j<len<<j++){
+		cout<<a;
+	}
+	return 0;
+}
