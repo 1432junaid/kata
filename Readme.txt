2c0aedf7418eb3539d50d8086c7fb809f74f62fd
1432junaid <1432junaid@gmail.com>
2020-02-25 15:52:22 +0530

    update readme.txt

---
+++ Readme.txt
@@ -0,0 +1,12 @@
+kata001	:	WAP to take input any alphabet in order 1 to 26 like 1=a and z=26 repeatedly aa=26+1 and zz = 26+26
+kata002	:	WAP to find the longest sub-sequence
+kata003	:	given a positive integer n,find out the smallest integer m, such that the product of all digits of m is equal to n.
+		// for ex :
+		// n = 36  return m = 49
+		// n = 72  return m = 89
+kata005	:	find the output
+kata006 :	WAP to perform all the operations on matrix
+kata007	:	WAP to find the frequency of letter in a string 
+
+
+
--- kata001.c
+++ kata001.c
@@ -1,13 +1,20 @@
 //WAP to take input any alphabet in order 1 to 26 like 1=a and z=26 repeatedly and aa=26+1,and zz=26+26
-
+//may be there is mistake in programe assume a=1 and z=26 then aa=1+1 and zz = 26+26
 
 #include<stdio.h>
+int count();
 int main(){
-	char any;
-	int data;
-	printf("Enter a alphabet\n");
-	scanf("%c",&any);
-	data=(int)(any);
-	printf("%d\n",any-96);
-	return 0;
+///	char any;
+	int ans =0;
+	int result = count();
+	int count(){
+//		printf("Enter a alphabet\n");
+		int any
+		scanf("%c",&any);
+		if(any != '\n'){
+			int data=(int)(any);
+			printf("%d\n",any-96);
+			ans += any-96;
+		}
+		return 0;
 }
